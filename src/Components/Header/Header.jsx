c91e6172e1724add0bbcbf00848cbfcf6e5796dc
Philip Lucas <126236947+stuckinsnow@users.noreply.github.com>
2023-07-18 09:56:57 -0700

    add navlink.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,29 @@
 import './Header.scss';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div id='header'>
-      <p className='navbar'>Header writing</p>
-
-      <div className="custom-shape-divider-bottom-1689659056">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" className="shape-fill"></path>
-        </svg>
-      </div>
+    <div id='header'> 
+      <Link id="headerlogo" to={"/"}>VanVisual</Link>
+      <nav>
+        <ul className='navbar'>
+          <li className='navbar__item'>
+            <NavLink to="/" activeclassname="active">
+              Home
+            </NavLink>
+          </li>
+          <li className='navbar__item'>
+            <NavLink to="/projects/" activeclassname="active">
+              Our Work
+            </NavLink>
+          </li> 
+          <li className='navbar__item'>
+            <NavLink to="/contact" activeclassname="active">
+              About Us
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
 
 
     </div>

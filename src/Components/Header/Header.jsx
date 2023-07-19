775921be83a5d0326cf8477accc3d5c230c490ab
stuckinsnow <126236947+stuckinsnow@users.noreply.github.com>
2023-07-19 14:55:31 -0700

    add animations to menu.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState, useRef } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { useToggleClass } from '../../utils/functions';
 
@@ -5,39 +6,62 @@ import './Header.scss';
 
 export default function Header() {
   const [isHamburgerOpen, toggleHamburger] = useToggleClass(false);
+  const [isHamburgerClicked, setIsHamburgerClicked] = useState(false);
+  const [delayedItems, setDelayedItems] = useState([]);
+  const delayedItemsTimeoutsRef = useRef([]);
 
   const handleNavLinkClick = () => {
     if (isHamburgerOpen) {
       toggleHamburger();
+      setIsHamburgerClicked(true);
     }
   };
 
+  useEffect(() => {
+    if (isHamburgerOpen) {
+      const navItems = document.querySelectorAll('.navlist__item');
+      const delayTime = 500;
+
+      delayedItemsTimeoutsRef.current = Array.from(navItems).map((item, index) =>
+        setTimeout(() => {
+          setDelayedItems((prevItems) => [...prevItems, index]);
+        }, delayTime * index)
+      );
+    } else {
+      setDelayedItems([]);
+    }
+
+    return () => {
+      delayedItemsTimeoutsRef.current.forEach((timeout) => clearTimeout(timeout));
+    };
+  }, [isHamburgerOpen]);
+
   return (
     <div id='header'>
-      <Link className="headerlogo" to={"/"}>
+      <Link className="headerlogo" to={"/"} onClick={handleNavLinkClick}>
         <span className='headerlogo__van'>Van</span><span className='headerlogo__visual'>Visual</span>
       </Link>
 
       {/* Navbar */}
       <nav className={`navbar ${isHamburgerOpen ? 'active' : ''}`}>
         <ul className='navlist'>
-          <li className='navlist__item navlist__item--request'>
+          <li className={`navlist__item navlist__item-1 navlist__item-1`}>
             <Link to="/contact" className="navlist__item--request" onClick={handleNavLinkClick}>
               Request a design
             </Link>
           </li>
-          <li className='navlist__item'>
-            <NavLink to="/" className="navlist__item--link" onClick={handleNavLinkClick}>
+          <li className={`navlist__item navlist__item-2 ${delayedItems.includes(1) ? 'delay' : ''}`}>
+            <NavLink to="/" className="navlist__item--link" onClick={handleNavLinkClick} >
               Home
             </NavLink>
           </li>
-          <li className='navlist__item'>
-            <NavLink to="/projects/" className="navlist__item--link" onClick={handleNavLinkClick}>
+          <li className={`navlist__item ${delayedItems.includes(2) ? 'delay' : ''}`}>
+            <NavLink to="/projects/" className="navlist__item--link" onClick={handleNavLinkClick} >
               Our Work
             </NavLink>
           </li>
-          <li className='navlist__item'>
-            <NavLink to="/about" className="navlist__item--link" onClick={handleNavLinkClick}>
+          <li className={`navlist__item ${delayedItems.includes(3) ? 'delay' : ''}`}>
+            <NavLink to="/about" className="navlist__item--link" onClick={handleNavLinkClick} >
               About Us
             </NavLink>
           </li>

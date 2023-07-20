cbbefe0f8b3915ab9fac525acbd1bf6012c6f12c
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-07-19 18:11:52 -0700

    implement footer flex.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
 import './Footer.scss';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
-        <div>
-            Footer
+        <div id='footer'>
+
+            <nav className='footernav'>
+
+                <ul className='footlist'>
+
+                    <li className='footlist__item'>
+                        <Link to="/projects/" className='footlist__item--link' >Our Work
+                        </Link>
+                    </li>
+                    <li className='footlist__item'>
+                        <Link to="/about" className='footlist__item--link' >About Us</Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 } 

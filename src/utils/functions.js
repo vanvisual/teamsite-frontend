8f97a0458ad0dabf7d666a13273b7ee8010a9703
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-07-20 09:01:25 -0700

    animate links.

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -19,3 +19,47 @@ export const useToggleClass = (initialState = false) => {
 
   return [isOpen, toggleClass, isHamburgerClicked, setIsHamburgerClicked];
 };
+
+export function animateLetters(className) {
+  function applyRandomTransform(letter) {
+    if (!letter.dataset.randomX || !letter.dataset.randomY) {
+      letter.dataset.randomX = Math.floor(Math.random() * 21 - 10) / 10;
+      letter.dataset.randomY = Math.floor(Math.random() * 11 - 10) / 10;
+    }
+
+    letter.style.transform = `translateX(${letter.dataset.randomX}rem) translateY(${letter.dataset.randomY}rem)`;
+  }
+
+  function removeRandomTransform(letter) {
+    letter.style.transform = 'none';
+  }
+
+  const links = document.querySelectorAll(className);
+
+  links.forEach((link) => {
+    const text = link.textContent;
+    link.innerHTML = '';
+
+    for (let i = 0; i < text.length; i++) {
+      const letterSpan = document.createElement('span');
+      letterSpan.classList.add('letter');
+      letterSpan.textContent = text[i] === ' ' ? '\u00A0' : text[i];
+      link.appendChild(letterSpan);
+    }
+
+    link.addEventListener('mouseover', () => {
+      const letters = link.querySelectorAll('.letter');
+      letters.forEach((letter) => {
+        applyRandomTransform(letter);
+      });
+    });
+
+    link.addEventListener('mouseout', () => {
+      const letters = link.querySelectorAll('.letter');
+      letters.forEach((letter) => {
+        removeRandomTransform(letter);
+      });
+    });
+  });
+}
+

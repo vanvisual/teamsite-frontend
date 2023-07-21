1bd5e1c5bb3263e309a2edb84b2cb225f0ea170c
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-07-20 23:02:03 -0700

    fix styling.

--- src/Components/HomeHero/HomeHero.jsx
+++ src/Components/HomeHero/HomeHero.jsx
@@ -3,41 +3,28 @@ import arrow from "../../assets/icons/arrow.png";
 
 const HomeHero = () => (
 
+    <div className="hero">
 
-    <>
-
-        <div className="hero">
-            <div className="hero__section">
-                <p className="hero__subtitle">Website Design</p>
-                <p className="hero__title">VanVisual</p>
-                <p className="hero__subtitle">We are a team of designers and developers dedicated to helping businesses create and build their onlone presence through visual appealing websites.</p>
+        <section className="hero__section">
+            <h1 id="hometitle" className="hero__title">VanVisual</h1>
+            <p className="hero__subtitle hero__subtitle--1">Website Design</p>
+            <p className="hero__subtitle">We are a team of designers and developers dedicated to helping businesses create and build their onlone presence through visual appealing websites.</p>
+        </section>
+        <div className="hero__section2">
+            <p className="hero__subtitle">OUR WORK</p>
+            <div className="hero__group">
+                <p className="hero__subtitle">Great work gets result</p>
+                <img src={arrow} alt="Arrow Icon" className="hero__icon" />
             </div>
-            <div className="hero__section2">
-                <p className="hero__subtitle">OUR WORK</p>
-                <div className="hero__group">
-                    <p className="hero__subtitle">Great work gets result</p>
-                    <img src={arrow} alt="Arrow Icon" className="hero__icon" />
-                </div>
-            </div>
-
-
-
-            <div class="custom-shape">
-                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M741,116.23C291,117.43,0,27.57,0,6V120H1200V6C1200,27.93,1186.4,119.83,741,116.23Z" class="shape-fill"></path>
-                </svg>
-            </div>
-
-
         </div>
 
+        <div className="custom-shape">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
+            </svg>
+        </div>
 
-
-
-
-
-
-    </>
+    </div>
 
 )
 

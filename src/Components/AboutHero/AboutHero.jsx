ae4dcc98ca05551b6d337ad58ebb6a88e2d1ecad
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-07-22 14:37:44 -0700

    change classnames.

--- src/Components/AboutHero/AboutHero.jsx
+++ src/Components/AboutHero/AboutHero.jsx
@@ -3,7 +3,7 @@ import './AboutHero.scss';
 const AboutHero = () => {
     return (
 
-        <div id="about-hero">
+        <div id='about-wrapper'>
             <section className="about">
                 <h1 className="about__title">Meet the team</h1>
                 <p className="about__writing--upper">We're a group of professionals raised by the internet and we want you to succeed.</p>

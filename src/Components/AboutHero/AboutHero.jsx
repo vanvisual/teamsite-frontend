53a47b1a4426cd140e8e6e2c5db000a76e343546
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-07-22 15:33:07 -0700

    improve styling.

--- src/Components/AboutHero/AboutHero.jsx
+++ src/Components/AboutHero/AboutHero.jsx
@@ -3,15 +3,15 @@ import './AboutHero.scss';
 const AboutHero = () => {
     return (
 
-        <div id='about-wrapper'>
-            <section className="about">
-                <h1 className="about__title">Meet the team</h1>
-                <p className="about__writing--upper">We're a group of professionals raised by the internet and we want you to succeed.</p>
+        <div id='c-abouthero'>
+            <section className="about-card">
+                <h1 className="about-card__title">Meet the team</h1>
+                <p className="about-card__description about-card__description--upper">We're a group of professionals raised by the internet and we want you to succeed.</p>
             </section>
 
-            <div className="about hidden-mobile">
-                <h2 className="about__title">Our Work</h2>
-                <p className="about__writing">Great work gets results</p>
+            <div className="about-card hidden-mobile">
+                <h2 className="about-card__title">Our Work</h2>
+                <p className="about-card__description">Great work gets results</p>
             </div>
         </div>
 

b50f87fe6251dcb494b9a641ca5d471efb5cc8a9
Neat <sophaneat@student.fdu.edu>
2023-08-11 12:32:00 -0700

    Move firebase config to .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,14 +7,14 @@ import {getDatabase} from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCO1XrfSS5Ec70-E02uvldD_mImWiQUTGs",
-    authDomain: "vanvisual-2023.firebaseapp.com",
-    databaseURL: "https://vanvisual-2023-default-rtdb.firebaseio.com",
-    projectId: "vanvisual-2023",
-    storageBucket: "vanvisual-2023.appspot.com",
-    messagingSenderId: "896915818912",
-    appId: "1:896915818912:web:8e4d45e82c18399c957cc5",
-    measurementId: "G-8R8LETQW9V"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
